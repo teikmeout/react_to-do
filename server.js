024aa08ac5be6db05067d42311a5a22afc0f8f37
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-11-07 14:56:41 -0500

    adding server functionality

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express       = require('express');
+const logger        = require('morgan');
+const bodyParser    = require('body-parser');
+
+// short circuit evaluation:
+// if NODE_ENV exists it will give us a false and will recognize we are in
+// heroku environment and not in DEV env
+// only load dotenv if we need it
+const isDev         = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
+
+const app           = express();
+
+const PORT          = process.argv[2] || process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server RUNNIN on port ${PORT}`));
+
+app.use(logger(isDev ? 'dev' : 'common'));
+
+// this is to only accept json in our local API
+app.use(bodyParser.json());
+
+// generic error handler
+app.use((err, req, res, next) => {
+  // if an error happens we want to be able to see what it is
+  console.log(err, next);
+  // but we want to tell the user there was a server issue.
+  res.status(500).send('Something broke!');
+});
+
