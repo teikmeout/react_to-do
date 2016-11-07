72299a950b660231ed0f6118ce3f7b5edc0f605d
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-11-07 16:14:36 -0500

    added schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,17 @@
-DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS task;
 
-CREATE TABLE tasks (
+CREATE TABLE task(
   id SERIAL PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
   description VARCHAR(255),
   completed BOOLEAN NOT NULL DEFAULT FALSE,
   deleted BOOLEAN NOT NULL DEFAULT FALSE,
   -- default value for this timestamp is the moment of creation
-  date_created TIMESTAMP NOT NULL DEFAULT NOW()
+  date_created TIMESTAMP NOT NULL DEFAULT NOW(),
   date_completed TIMESTAMP,
   date_deleted TIMESTAMP
 );
 
 -- create an index to facilitate searching through the DB
-CREATE INDEX
+CREATE INDEX name_idx
+ON task (name);
