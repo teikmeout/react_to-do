cb95d8ed80c58e2f6e8aba45d442d5b829d2c237
Jason Seminara <jseminara@gmail.com>
2016-11-07 09:47:36 -0500

    updated eslint settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-/* eslint no-multi-spaces: ["error", { exceptions: { "VariableDeclarator": true } }] */
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true }] */
 const webpack           = require('webpack');
 const path              = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -10,17 +8,16 @@ const BUILD_DIR         = path.resolve(__dirname, 'dist');
 const APP_DIR           = path.resolve(__dirname, 'src/client/app');
 
 const config = {
-  entry: `${APP_DIR}/main.js`,
+  entry:  `${APP_DIR}/main.js`,
   output: {
-    path: BUILD_DIR,
+    path:     BUILD_DIR,
     filename: '/js/[name].js',
   },
-
-  cache: true,
-  debug: true,
+  cache:   true,
+  debug:   true,
   devtool: 'eval-source-map',
-  stats: {
-    colors: true,
+  stats:   {
+    colors:  true,
     reasons: true,
   },
 
@@ -31,10 +28,10 @@ const config = {
       },
     }),
     new HtmlWebpackPlugin({
-      title: 'Tasks',
-      xhtml: true,
-      inject: false,
-      template: htmlTemplate,
+      title:      'Tasks',
+      xhtml:      true,
+      inject:     false,
+      template:   htmlTemplate,
       appMountId: 'container',
     }),
     new ExtractTextPlugin('/css/[name].css', {
@@ -49,19 +46,19 @@ const config = {
       { test: /\.ico$/, loader: 'file-loader?name=/[name].[ext]' },
       { test: /\.jsx?$/, loader: 'babel' },
       {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        test:   /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url-loader?limit=100&mimetype=application/font-woff&name=/fonts/[name].[ext]',
       },
       {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        test:   /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url-loader?limit=100&mimetype=application/octet-stream&name=/fonts/[name].[ext]',
       },
       {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        test:   /\.eot(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'file-loader?name=/fonts/[name].[ext]',
       },
       {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        test:   /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url-loader?limit=100&mimetype=image/svg+xml&name=/fonts/[name].[ext]',
       },
     ],
