2223d6ca3da9d31a03f97afa1e55ea067a39bf6a
Jason Seminara <jseminara@gmail.com>
2016-11-06 20:32:59 -0500

    cleaned up webpack config eslint-style; removed bootstrap dep

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
-'use strict'
+/* eslint no-multi-spaces: ["error", { exceptions: { "VariableDeclarator": true } }] */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true }] */
 const webpack           = require('webpack');
 const path              = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const htmlTemplate      = require('html-webpack-template');
 
 const BUILD_DIR         = path.resolve(__dirname, 'dist');
 const APP_DIR           = path.resolve(__dirname, 'src/client/app');
@@ -19,76 +21,73 @@ const config = {
   devtool: 'eval-source-map',
   stats: {
     colors: true,
-    reasons: true
+    reasons: true,
   },
 
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-      }
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+      },
     }),
     new HtmlWebpackPlugin({
       title: 'Tasks',
       xhtml: true,
       inject: false,
-      template: require('html-webpack-template'),
-      appMountId: 'container'
+      template: htmlTemplate,
+      appMountId: 'container',
     }),
     new ExtractTextPlugin('/css/[name].css', {
-      allChunks: true
-    })
+      allChunks: true,
+    }),
   ],
 
-  module : {
+  module: {
     loaders: [
-      { test: /\.css$/,  loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-      { test: /\.(png|gif|jpg)$/,  loader: 'file-loader?name=/images/[name].[ext]' },
-      { test: /\.ico$/,  loader: 'file-loader?name=/[name].[ext]' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+      { test: /\.(png|gif|jpg)$/, loader: 'file-loader?name=/images/[name].[ext]' },
+      { test: /\.ico$/, loader: 'file-loader?name=/[name].[ext]' },
       { test: /\.jsx?$/, loader: 'babel' },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=100&mimetype=application/font-woff&name=/fonts/[name].[ext]'
+        loader: 'url-loader?limit=100&mimetype=application/font-woff&name=/fonts/[name].[ext]',
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=100&mimetype=application/octet-stream&name=/fonts/[name].[ext]'
+        loader: 'url-loader?limit=100&mimetype=application/octet-stream&name=/fonts/[name].[ext]',
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader?name=/fonts/[name].[ext]'
+        loader: 'file-loader?name=/fonts/[name].[ext]',
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=100&mimetype=image/svg+xml&name=/fonts/[name].[ext]'
-      }
-    ]
-  }
+        loader: 'url-loader?limit=100&mimetype=image/svg+xml&name=/fonts/[name].[ext]',
+      },
+    ],
+  },
 };
 
-if( process.env &&
+if (process.env &&
   process.env.NODE_ENV &&
-  process.env.NODE_ENV == 'production'){
-
+  process.env.NODE_ENV === 'production') {
   const prodPlugins = [
     new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
+      compress: {
+        warnings: true,
       },
       output: {
         comments: false,
-      }
+      },
     }),
-    new webpack.optimize.CommonsChunkPlugin('/js/common.js')
-  ]
+    new webpack.optimize.CommonsChunkPlugin('/js/common.js'),
+  ];
 
-  config.plugins = config.plugins.concat(prodPlugins)
+  config.plugins = config.plugins.concat(prodPlugins);
 
-  config.cache= false;
-  config.debug= false;
-  config.devtool= undefined;
-
-
+  config.cache = false;
+  config.debug = false;
+  config.devtool = undefined;
 }
 
- module.exports = config;
+module.exports = config;
